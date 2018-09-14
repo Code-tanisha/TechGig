b5ab834554dd0cf49b8a207156f4519fa10b1bcd
Tanisha Gupta <37878240+Tanisha2910@users.noreply.github.com>
2018-09-14 21:55:17 +0530

    Program is to print Square pattern .

---
+++ Sq.pattern.java
@@ -0,0 +1,17 @@
+//This Program is to print Square pattern.
+
+import java.io.*;
+import java.util.*;
+public class CandidateCode {
+   public static void main(String args[] ) throws Exception {
+	for(int i = 0; i<5; i++){
+	    for(int j = 0; j<5; j++){
+	        if(j==4)
+	        System.out.print("* ");
+	        else
+	        System.out.print("* ");
+	    }
+	    System.out.println();
+	}
+ }
+}
